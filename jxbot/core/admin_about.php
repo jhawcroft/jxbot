68522f69c651e512e5768a99201cd19afb97db34
Josh Hawcroft <dev@joshhawcroft.org>
2015-12-25 16:00:04 +1000

    amended about page to link to AIML/Alice

--- jxbot/core/admin_about.php
+++ jxbot/core/admin_about.php
@@ -1,4 +1,4 @@
-<p>A light-weight natural-language chat system<br>
+<p>A light-weight natural-language chat system based upon <a href="http://www.alicebot.org/aiml.html">Alice and AIML</a><br>
 by <a href="http://joshhawcroft.org/">Joshua Hawcroft</a></p>
 
 <p><strong>version <?php print JxBot::VERSION; ?></strong></p>
