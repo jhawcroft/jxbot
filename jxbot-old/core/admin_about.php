e918a3275ee6accda74b823176d14918d4a22c9a
Josh Hawcroft <dev@joshhawcroft.org>
2015-12-26 16:05:01 +1000

    removed old files

--- jxbot-old/core/admin_about.php
+++
@@ -1,6 +0,0 @@
-<p>A light-weight natural-language chat system<br>
-by <a href="http://joshhawcroft.org/">Joshua Hawcroft</a></p>
-
-<p><strong>version <?php print BotDefaults::VERSION; ?></strong></p>
-
-<?php JxBotUtil::phpinfo(); ?>
--- jxbot-old/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__).'/core/common.php');
-require_once(dirname(__FILE__).'/core/admin.php');
-
