08b5d35100574fe9438b267476add71317dd1e07
Josh Hawcroft <dev@joshhawcroft.org>
2015-12-29 16:30:01 +1000

    primitive bulk reload; needs further work

--- jxbot/core/admin_import.php
+++ jxbot/core/admin_import.php
@@ -84,25 +84,86 @@ function page_import_form()
 {
 ?>
 
-<?php if (isset($_FILES['data_file'])) do_handle_upload(); ?>
+<h2>Bulk Reload</h2>
+
+<?php
+
+if (isset($_POST['action']) && ($_POST['action'] == 'bulk-reload'))
+	bulk_reload();
+
+?>
+
+<p><button type="submit" name="action" value="bulk-reload">Purge & Reload</button></p>
+
+
+
+<h2>Individual File</h2>
+
+<?php if (isset($_FILES['data_file']) && ($_POST['action'] == 'upload')) do_handle_upload(); ?>
 
 <p><label for="data_file">File:</label>
 <input type="file" name="data_file" id="data_file" size="50"></p>
 
-<p><?php JxWidget::button('Upload', 'upload', ''); ?></p>
+<p><button type="submit" name="action" value="upload">Upload File</button></p>
 
-<hr>
 
 
 <h2>Purge</h2>
 
-<p><button name="action" value="purge">Purge Categories</button></p>
+<p><button type="submit" name="action" value="purge">Purge Categories</button></p>
+
+
 
 <?php
 }
 
 
+function bulk_reload()
+{
 
+// regarding sending updates to the browser:
+// best bet is to update the file listing in a table,
+// put status: loaded, errors, loading into the table for each file
+// have a periodic refresh that enquires as to the upload progress
 
+// another option,
+// provide the list, and let javascript run through the list
+// and send an ajax request/normal page request for each
+
+	print '<p>';
+	
+	print 'Purging existing database...';
+	JxBotNLData::purge_categories();
+	
+	print '<span class="green">DONE</span>. <br>';
+
+	$aiml_directory = dirname(dirname(__FILE__)).'/aiml/';
+	
+	$dh = opendir($aiml_directory);
+	while (($file = readdir($dh)) !== false)
+	{
+		if (substr($file, 0, 1) == '.') continue;
+		if (pathinfo($file)['extension'] != 'aiml') continue;
+		
+		print 'Importing '.$file.'...';
+		
+		$filename = $aiml_directory . $file;
+		$importer = new JxBotAimlImport();
+		$result = $importer->import($filename);
+		
+		if (is_array($result)) 
+		{
+			print '<span class="green">DONE</span><br>';
+			print implode('<br>', $result);
+		}
+		else
+			print '<span class="red">'.$result.'</span>';
+		
+		print '<br>';
+	}
+	closedir($dh);
+	
+	print '</p>';
+}
 
 
